947119a0644a66984385d4841449fdf7ed7b4353
hbarnardt <hbarnardt@retrorabbit.co.za>
2015-10-13 14:40:51 +0200

    Added search pages

--- harambee/urls.py
+++ harambee/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^menu/$', 'harambee.core_views.menu', name='misc.menu'),
+    url(r'^search/$', 'harambee.core_views.search', name='misc.search'),
+    url(r'^search_results/$', 'harambee.core_views.search_results', name='misc.search_results'),
 
     url(r'^$', 'harambee.misc_views.welcome', name='misc.welcome'),
     url(r'^why_id/$', 'harambee.misc_views.why_id', name='misc.why_id'),
@@ -13,8 +15,8 @@ urlpatterns = [
     url(r'^contact/$', 'harambee.misc_views.contact', name='misc.contact'),
     url(r'^about/$', 'harambee.misc_views.about', name='misc.about'),
     url(r'^intro/$', 'harambee.misc_views.intro', name='misc.intro'),
-    url(r'^help/$', 'harambee.misc_views.help', name='misc.help'),
-    url(r'^help_page//(?P<page_id>\d+)$', 'harambee.misc_views.help_page', name='misc.help_page'),
+    url(r'^help/$', 'harambee.misc_views.main_help', name='misc.help'),
+    url(r'^help_page/(?P<page_id>\d+)$', 'harambee.misc_views.help_page', name='misc.help_page'),
 
     url(r'^join/$', 'harambee.auth_views.join', name='auth.join'),
     url(r'^login/$', 'harambee.auth_views.login', name='auth.login'),
