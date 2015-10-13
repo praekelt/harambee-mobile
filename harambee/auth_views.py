f51568ed7172a2d802396148a4192446a316cee7
hbarnardt <hbarnardt@retrorabbit.co.za>
2015-10-13 10:27:14 +0200

    Changed log in form

--- harambee/auth_views.py
+++ harambee/auth_views.py
@@ -26,7 +26,7 @@ def join(request):
     return resolve_http_method(request, [get, post])
 
 
-def login(request, state):
+def login(request):
 
     try:
         page_model = Page.objects.get(lookup="login")
@@ -36,7 +36,7 @@ def login(request, state):
         page = {'title': 'LOG IN', 'heading': 'LOG IN'}
 
     def get():
-        return render(request, "auth/login.html", {"page": page, "state": state,
+        return render(request, "auth/login.html", {"page": page,
                                                    "form": LoginForm()})
 
     def post():
@@ -57,68 +57,12 @@ def login(request, state):
                 username=form.cleaned_data["username"]
             ).exists()
             request.session["user_exists"] = exists
-
-            if user is not None and user.is_active:
-                try:
-                    registered = is_registered(user)
-                    class_active = is_class_active(user)
-                    if registered is not None and class_active:
-                        save_user_session(request, registered, user)
-
-                        usr = Learner.objects.filter(username=form.cleaned_data["username"])
-                        par = Participant.objects.filter(learner=usr, is_active=True)
-
-                        if len(par) > 1:
-                            subject = ' '.join([
-                                'Multiple participants active -',
-                                usr.first().first_name,
-                                usr.first().last_name,
-                                '-',
-                                usr.first().username
-                            ])
-                            message = '\n'.join([
-                                'Student: ' + usr.first().first_name + ' ' + usr.first().last_name,
-                                'Msisdn: ' + usr.first().username,
-                                'is unable to login due to having multiple active participants.',
-                                'To fix this, some participants  will have to be deactivated.'
-                            ])
-
-                            mail_managers(
-                                subject=subject,
-                                message=message,
-                                fail_silently=False
-                            )
-                            return render(request, "misc/account_problem.html")
-
-                        event = Event.objects.filter(course=par.first().classs.course,
-                                                     activation_date__lte=datetime.now(),
-                                                     deactivation_date__gt=datetime.now()
-                                                     ).first()
-
-                        if event:
-                            allowed, event_participant_rel = par.first().can_take_event(event)
-                            if allowed:
-                                return redirect("learn.event_splash_page")
-
-                        if ParticipantQuestionAnswer.objects.filter(participant=par).count() == 0:
-                            return redirect("learn.first_time")
-                        else:
-                            return redirect("learn.home")
-                    else:
-                        return redirect("auth.getconnected")
-                except ObjectDoesNotExist:
-                    request.session["wrong_password"] = False
-                    return redirect("auth.getconnected")
-            else:
-                # Save provided username
-                request.session["user_lockout"] = False
-                request.session["username"] = form.cleaned_data["username"]
-                request.session["wrong_password"] = True
-                return redirect("auth.getconnected")
+            return redirect("misc.intro")
         else:
             return get()
 
     return resolve_http_method(request, [get, post])
+
 
 def forgot_pin(request):
 
