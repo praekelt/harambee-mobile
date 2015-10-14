8d9a286f1192d550f1db22d9ced6d085dcfb91ce
msinovcic <msinovcic@retrorabbit.co.za>
2015-10-14 09:38:44 +0200

    Added PageAdmin and HelpPageAdmin
    Made changes to Page and HelpPage Models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,33 @@
+from datetime import datetime
 from django.contrib import admin
+from core.models import Page, HelpPage
 
-# Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("name", "title", "heading", "content")
+    ordering = ["name"]
+    search_fields = ("name",)
+
+    fieldsets = [
+        (None, {"fields": ["name", "title", "heading", "content"]}),
+    ]
+
+
+class HelpPageAdmin(admin.ModelAdmin):
+    list_display = ("name", "title", "heading", "content", "is_live")
+    ordering = ["name"]
+    search_fields = ("name",)
+
+    fieldsets = [
+        ("Content", {"fields": ["name", "heading", "content"]}),
+        ("Promotion", {"fields": ["slug", "title" "show", "description"]}),
+        ("Settings", {"fields": ["activate", "deactivate"]}),
+    ]
+
+    def is_live(self, obj):
+        return (obj.activate is not None and obj.activate > datetime.now()) and \
+               (obj.deactivate is None or obj.deactivate < datetime.now())
+    is_live.short_description = 'Active'
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(HelpPage, HelpPageAdmin)
