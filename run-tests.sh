7c848ce67d53a02a8ea37352a01bb8a0e7cbe456
hbarnardt <hbarnardt@retrorabbit.co.za>
2015-10-19 09:16:24 +0200

    Updated run test to work with django 1.8

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 python manage.py syncdb | tee
 python manage.py migrate
-python manage.py syncdb --all --noinput  # call AFTER migrations for permission creation
 coverage run --source=harambee manage.py test
