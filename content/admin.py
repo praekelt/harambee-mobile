a28b31c5b525e4e15993a26388b7e04f844867f5
msinovcic <msinovcic@retrorabbit.co.za>
2015-10-21 11:33:47 +0200

    Made database changes

--- content/admin.py
+++ content/admin.py
@@ -1,21 +1,14 @@
 from django.contrib import admin
-from content.models import Journey, Module, Level, LevelQuestion, LevelQuestionOption
+from content.models import Journey, Module, Level, LevelQuestion, LevelQuestionOption, JourneyModuleRel
 
 
-class LevelQuestionInline(admin.StackedInline):
-    model = LevelQuestion
+class CourseModuleInline(admin.TabularInline):
+    model = JourneyModuleRel
     extra = 1
-    fields = ("name", "description", "order", "level", "question_content", "answer_content", "notes", "image")
-
-
-class LevelQuestionOptionInline(admin.StackedInline):
-    model = LevelQuestionOption
-    extra = 1
-    fields = ("name", "question", "order", "content", "correct")
 
 
 class JourneyAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug", "show_menu", "title", "intro_text", "search", "start_date", "end_date")
+    list_display = ("name", "show_menu", "start_date", "end_date",)
     ordering = ["slug"]
     search_fields = ("slug",)
 
@@ -25,22 +18,25 @@ class JourneyAdmin(admin.ModelAdmin):
         ("Settings", {"fields": ["start_date", "end_date"]}),
     ]
 
+    inlines = (CourseModuleInline,)
+
 
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ("name", "intro_text", "end_text", "get_journeys", "accessibleTo", "show_recommended", "slug",
-                    "title", "show_menu", "search", "minimum_questions", "minimum_percentage", "store_data_per_user",
+    list_display = ("name", "get_journeys", "accessibleTo", "minimum_questions", "minimum_percentage",
                     "start_date", "end_date", "publish_date")
 
     fieldsets = [
         (None, {"fields": ["name", "intro_text", "end_text"]}),
-        ("Promotion", {"fields": ["journeys", "accessibleTo", "show_recommended", "slug", "title", "show_menu",
+        ("Promotion", {"fields": ["accessibleTo", "show_recommended", "slug", "title", "show_menu",
                                   "search"]}),
         ("Settings", {"fields": ["minimum_questions", "minimum_percentage", "store_data_per_user", "start_date",
                                  "end_date"]}),
     ]
 
-    def get_journeys(self, module):
-        journeys = module.journeys.all()
+    inlines = (CourseModuleInline,)
+
+    def get_journeys(self, object):
+        journeys = object.journeys.all()
 
         journeys_list = ""
 
@@ -48,8 +44,13 @@ class ModuleAdmin(admin.ModelAdmin):
             journeys_list += j.name + "\n"
 
         return journeys_list
-
     get_journeys.short_description = "Journeys"
+
+
+class LevelQuestionInline(admin.StackedInline):
+    model = LevelQuestion
+    extra = 1
+    fields = ("name", "description", "order", "level", "question_content", "answer_content", "notes", "image")
 
 
 class LevelAdmin(admin.ModelAdmin):
@@ -60,6 +61,12 @@ class LevelAdmin(admin.ModelAdmin):
     ]
 
     inlines = (LevelQuestionInline,)
+
+
+class LevelQuestionOptionInline(admin.StackedInline):
+    model = LevelQuestionOption
+    extra = 1
+    fields = ("name", "question", "order", "content", "correct")
 
 
 class LevelQuestionAdmin(admin.ModelAdmin):
--- content/models.py
+++ content/models.py
@@ -1,22 +1,11 @@
 from django.db import models
 from django.db.models import Count
 
-ACTIVE = 0
-PASSED = 1
-COMPLETE = 2
-
-STATE_CHOICES = (
-    (ACTIVE, "Active"),
-    (PASSED, "Passed"),
-    (COMPLETE, "Complete"),
-)
-
 
 class Journey(models.Model):
 
     name = models.CharField("Name", max_length=500, blank=False, unique=True)
     intro_text = models.TextField("Introductory Text", blank=True)
-
     slug = models.SlugField("Slug", unique=True)
     title = models.CharField("Title", max_length=500, blank=False)
     show_menu = models.BooleanField("Show in menus", default=True)
@@ -24,6 +13,8 @@ class Journey(models.Model):
     image = models.ImageField("Image", upload_to="img/", blank=True, null=True)
     start_date = models.DateTimeField("Go Live On", null=True, blank=True)
     end_date = models.DateTimeField("Expire On", null=True, blank=True)
+    publish_date = models.DateTimeField("Published On", null=False, blank=False, auto_now_add=True)
+    modified_date = models.DateTimeField("Last Modified", null=False, blank=False, auto_now=True)
 
     def __unicode__(self):
         return self.name
@@ -59,7 +50,8 @@ class Module(models.Model):
     intro_text = models.TextField("Introductory Text", blank=True)
     end_text = models.TextField("Complete Page Text", blank=True)
     image = models.ImageField("Image", upload_to="img/", blank=True, null=True)
-    journeys = models.ManyToManyField(Journey)
+    journeys = models.ManyToManyField(
+        Journey, related_name='modules', through='JourneyModuleRel',)
     accessibleTo = models.PositiveIntegerField(
         "Accessible To", choices=(
             (ALL, "All"),
@@ -68,13 +60,17 @@ class Module(models.Model):
         ),
         default=ALL)
     show_recommended = models.BooleanField("Feature in Recommended for You", default=True)
-    slug = models.SlugField("Slug", unique=True)
-    title = models.CharField("Page Title", max_length=500, blank=False)
-    show_menu = models.BooleanField("Show in menus", default=True)
-    search = models.CharField("Search description", max_length=500)
-    minimum_questions = models.PositiveIntegerField("Minimum questions answered")
+    slug = models.SlugField("Slug", unique=True, help_text="Slug used to identify this module in URL. Must be unique.")
+    title = models.CharField("Page Title", max_length=500, blank=False,
+                             help_text="Title is displayed in the browsers tab.")
+    show_menu = models.BooleanField("Show in menu", default=True, help_text="Show the module link in users menu?")
+    search = models.CharField("Search description", max_length=500,)
+    minimum_questions = models.PositiveIntegerField("Minimum # of questions to answer",
+                                                    help_text="Required number of questions to be answered to pass a "
+                                                              "level.")
     minimum_percentage = models.PositiveIntegerField(
-        "Minimum % gained for all questions answered", choices=PERCENTAGE_CHOICES)
+        "Minimum overall % for all questions answered", choices=PERCENTAGE_CHOICES,
+        help_text="Required overall % to pass a level.")
     store_data_per_user = models.BooleanField("Data stored against User I.D.", default=True)
     start_date = models.DateTimeField("Go Live On", null=True, blank=True)
     end_date = models.DateTimeField("Expire On", null=True, blank=True)
@@ -95,6 +91,29 @@ class Module(models.Model):
 
     def slug_end(self):
         return "module_end/%s" % self.slug
+
+
+class JourneyModuleRel(models.Model):
+
+    journey = models.ForeignKey(Journey)
+    module = models.ForeignKey(Module)
+
+
+class HarambeeJourneyModuleRel(models.Model):
+
+    MODULE_ACTIVE = 0
+    MODULE_COMPLETE = 1
+
+    MODULE_STATE_CHOICES = (
+        (MODULE_ACTIVE, "Active"),
+        (MODULE_COMPLETE, "Complete"),
+    )
+
+    harambee = models.ForeignKey('my_auth.Harambee', null=False, blank=False)
+    journey_module_rel = models.ForeignKey(JourneyModuleRel, null=False, blank=False)
+    state = models.PositiveIntegerField(choices=MODULE_STATE_CHOICES, default=MODULE_ACTIVE)
+    date_started = models.DateTimeField("Date Started", auto_now_add=True, null=True, blank=True)
+    date_completed = models.DateTimeField("Date Completed", null=True, blank=True)
 
 
 class Level(models.Model):
@@ -140,6 +159,26 @@ class LevelQuestion(models.Model):
         ordering = ['order']
 
 
+class HarambeeJourneyModuleLevelRel(models.Model):
+
+    LEVEL_ACTIVE = 0
+    LEVEL_COMPLETE = 1
+
+    LEVEL_STATE_CHOICES = (
+        (LEVEL_ACTIVE, "Active"),
+        (LEVEL_COMPLETE, "Complete"),
+    )
+
+    harambee_journey_module_rel = models.ForeignKey(HarambeeJourneyModuleRel, null=True, blank=False)
+    level = models.ForeignKey(Level, null=False, blank=False)
+    state = models.PositiveIntegerField(choices=LEVEL_STATE_CHOICES, default=LEVEL_ACTIVE)
+    level_passed = models.BooleanField(default=False)
+    date_started = models.DateTimeField("Date Started", auto_now_add=True, null=True, blank=True)
+    date_completed = models.DateTimeField("Date Completed", null=True, blank=True)
+    level_attempt = models.PositiveIntegerField("Attempt Number")
+    current_question = models.ForeignKey(LevelQuestion, null=True, blank=True)
+
+
 class LevelQuestionOption(models.Model):
 
     name = models.CharField("Name", max_length=500, null=True, blank=False, unique=True)
@@ -156,48 +195,19 @@ class LevelQuestionOption(models.Model):
         verbose_name_plural = "Question Options"
 
 
-class HarambeeModuleRel(models.Model):
-
-    MODULE_ACTIVE = 0
-    MODULE_COMPLETED = 1
-
-    MODULE_STATE_CHOICES = (
-        (MODULE_ACTIVE, "Active"),
-        (MODULE_COMPLETED, "Completed")
-    )
-
-    harambee = models.ForeignKey('my_auth.Harambee', null=False, blank=False)
-    module = models.ForeignKey(Module, null=False, blank=False)
-
-    #journey_module_rel = models.ForeignKey(JourneyModuleRel, null=False, blank=False)
-    # state = models.PositiveIntegerField(blank=False, choices=MODULE_STATE_CHOICES)
-    # attempt = models.PositiveIntegerField(blank=False)
-
-
-class HarambeeLevelRel(models.Model):
-
-    harambee = models.ForeignKey('my_auth.Harambee', null=False, blank=False)
-    level = models.ForeignKey(Level, null=False, blank=False)
-    state = models.PositiveIntegerField(choices=STATE_CHOICES, default=ACTIVE)
-    date_started = models.DateTimeField("Date Started", auto_now_add=True, null=True, blank=True)
-    date_completed = models.DateTimeField("Date Completed", null=True, blank=True)
-    attempt = models.PositiveIntegerField("Attempt Number")
-
-
 class HarambeeQuestionAnswer(models.Model):
 
     harambee = models.ForeignKey('my_auth.Harambee', null=False, blank=False)
     question = models.ForeignKey(LevelQuestion, null=False, blank=False)
     option_selected = models.ForeignKey(LevelQuestionOption, null=False, blank=False)
     date_answered = models.DateTimeField(null=False, blank=False)
-    harambee_level_rel = models.ForeignKey(HarambeeLevelRel, null=False, blank=False)
+    harambee_level_rel = models.ForeignKey(HarambeeJourneyModuleLevelRel, null=False, blank=False)
 
     class Meta:
         verbose_name = "Level Question Answer"
         verbose_name_plural = "Level Question Answers"
 
 
-class JourneyModuleRel(models.Model):
-
-    journey = models.ForeignKey(Journey)
-    module = models.ForeignKey(Module)
+class HarambeeState(models.Model):
+    harambee = models.ForeignKey('my_auth.Harambee', null=False, blank=False)
+    active_level_rel = models.ForeignKey(HarambeeJourneyModuleLevelRel, null=True, blank=True)
