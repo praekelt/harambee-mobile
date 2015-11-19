148f39e9bc1d6e3a7b7f050bb12b939cfb6b8282
dbreedt <dbreedt@retrorabbit.co.za>
2015-11-19 21:38:26 +0200

    more changes

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,10 +3,10 @@
 pwd
 cp id_rsa.travis ~/.ssh
 echo Host github.com >> ~/.ssh/config
-echo "\tHostname ssh.github.com" >> ~/.ssh/config
-echo "\tUser git" >> ~/.ssh/config
-echo "\tPort 443" >> ~/.ssh/config
-echo "\tIdentityFile ~/.ssh/id_rsa.travis" >> ~/.ssh/config
+echo -e "\tHostname ssh.github.com" >> ~/.ssh/config
+echo -e "\tUser git" >> ~/.ssh/config
+echo -e "\tPort 443" >> ~/.ssh/config
+echo -e "\tIdentityFile ~/.ssh/id_rsa.travis" >> ~/.ssh/config
 
 ls ~/.ssh
 cat ~/.ssh/config
