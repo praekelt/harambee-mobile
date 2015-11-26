1130e1a4984f5342967e3fa7ca8835220e83f573
msinovcic <msinovcic@retrorabbit.co.za>
2015-11-26 16:34:15 +0200

    Fixed metrics

--- harambee/metrics.py
+++ harambee/metrics.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from datetime import datetime, timedelta
+from django.utils import timezone
 from my_auth.models import Harambee, HarambeeLog
 from content.models import HarambeeJourneyModuleRel, HarambeeJourneyModuleLevelRel, LevelQuestion, \
     HarambeeQuestionAnswer, Level, JourneyModuleRel, HarambeeeQuestionAnswerTime
@@ -11,24 +12,24 @@ def get_number_registered_users():
 
 
 def get_number_active_users_today():
-    now = datetime.now()
-    yesterday = datetime.datetime.now() - timedelta(hours=24)
+    now = timezone.now()
+    yesterday = timezone.now() - timedelta(hours=24)
     return Harambee.objects.filter(last_login__gt=yesterday, last_login__lt=now).aggregate(Count('id'))['id__count']
 
 
 def get_number_inactive_users():
-    two_weeks_ago = datetime.datetime.now() - timedelta(days=14)
+    two_weeks_ago = timezone.now() - timedelta(days=14)
     return Harambee.objects.filter(last_login__lt=two_weeks_ago).aggregate(Count('id'))['id__count']
 
 
 def get_active_user_ids_today():
-    now = datetime.now()
-    yesterday = datetime.datetime.now() - timedelta(hours=24)
+    now = timezone.now()
+    yesterday = timezone.now() - timedelta(hours=24)
     return Harambee.objects.filter(last_login__gt=yesterday, last_login__lt=now).values_list('id', flat=True)
 
 
 def get_number_active_users_per_module(journey_module_rel):
-    two_weeks_ago = datetime.datetime.now() - timedelta(days=14)
+    two_weeks_ago = timezone.now() - timedelta(days=14)
     return HarambeeQuestionAnswer.objects.filter(
         harambee_level_rel__harambee_journey_module_rel__journey_module_rel=journey_module_rel,
         date_answered__gt=two_weeks_ago)\
@@ -36,7 +37,7 @@ def get_number_active_users_per_module(journey_module_rel):
 
 
 def get_active_user_ids_per_module(journey_module_rel):
-    two_weeks_ago = datetime.datetime.now() - timedelta(days=14)
+    two_weeks_ago = timezone.now() - timedelta(days=14)
     return HarambeeQuestionAnswer.objects.filter(
         harambee_level_rel__harambee_journey_module_rel__journey_module_rel=journey_module_rel,
         date_answered__gt=two_weeks_ago).values_list('harambee__id', flat=True)
@@ -45,7 +46,8 @@ def get_active_user_ids_per_module(journey_module_rel):
 def get_number_active_logged_in_users_today_per_module(journey_module_rel):
     active_user_ids_per_module = get_active_user_ids_per_module(journey_module_rel)
     active_user_ids_today = get_active_user_ids_today()
-    return Harambee.objects.filter(id__in=active_user_ids_per_module, id__in=active_user_ids_today)\
+    joined = list(active_user_ids_per_module) + list(active_user_ids_today)
+    return Harambee.objects.filter(id__in=joined)\
         .aggregate(Count('id'))['id__count']
 
 
@@ -55,13 +57,13 @@ def get_number_registered_users_per_module(journey_module_rel):
 
 
 def get_number_completed_users_per_module(journey_module_rel):
-    now = datetime.now()
+    now = timezone.now()
     return HarambeeJourneyModuleRel.objects.filter(journey_module_rel=journey_module_rel, date_completed__lt=now)\
         .aggregate(Count('id'))['id__count']
 
 
 def get_number_active_users_per_level(level):
-    two_weeks_ago = datetime.datetime.now() - timedelta(days=14)
+    two_weeks_ago = timezone.now() - timedelta(days=14)
     return HarambeeJourneyModuleLevelRel.objects.filter(level=level, last_active__gt=two_weeks_ago)\
         .aggregate(Count('id'))['id__count']
 
@@ -86,13 +88,15 @@ def get_number_passed_users_per_level_module(journey_module_rel):
 
 def get_correct_percentage_per_level(level):
     question_ids = LevelQuestion.objects.filter(level=level).values_list('id', flat=True)
-    correct = HarambeeQuestionAnswer.objects.filter(question__id__in=question_ids, correct=True)\
+    correct = HarambeeQuestionAnswer.objects.filter(question__id__in=question_ids, option_selected=True)\
         .aggregate(Count('id'))['id__count']
     return correct / level.get_num_questions() * 100
 
 
 def get_average_correct_percentage_per_module_levels(journey_module_rel):
     num_levels = Level.objects.filter(module=journey_module_rel.module).aggregate(Count('id'))['id__count']
+    if num_levels == 0:
+        return 0
     levels = Level.objects.filter(module=journey_module_rel.module)
     total = 0
     for level in levels:
@@ -109,6 +113,8 @@ def get_num_questions_answered_per_level(level):
 
 def get_average_num_questions_answered_per_module_levels(journey_module_rel):
     num_levels = Level.objects.filter(module=journey_module_rel.module).aggregate(Count('id'))['id__count']
+    if num_levels == 0:
+        return 0
     levels = Level.objects.filter(module=journey_module_rel.module)
     total = 0
     for level in levels:
@@ -118,7 +124,9 @@ def get_average_num_questions_answered_per_module_levels(journey_module_rel):
 
 def get_average_time_per_level(level):
     times = HarambeeeQuestionAnswerTime.objects.filter(harambee_level_rel__level=level)
-    total_time = 0;
+    if times.aggregate(Count('id'))['id__count'] == 0:
+        return 0
+    total_time = 0
     for time in times:
         total_time += time.answer_time_in_minutes()
     return total_time/times.aggregate(Count('id'))['id__count']
@@ -134,6 +142,8 @@ def get_average_time_per_level_per_module(journey_module_rel):
 
 def get_average_time_per_module(journey_module_rel):
     levels = journey_module_rel.module.get_levels()
+    if levels.aggregate(Count('id'))['id__count'] == 0:
+        return 0
     total_time = 0
     for level in levels:
         total_time += get_average_time_per_level(level)
@@ -142,23 +152,30 @@ def get_average_time_per_module(journey_module_rel):
 
 def get_number_active_modules_per_harambee(harambee):
     return HarambeeJourneyModuleRel.objects.filter(harambee=harambee, state=HarambeeJourneyModuleRel.MODULE_ACTIVE)\
-        .aggregate(Count('id'))['count__id']
+        .aggregate(Count('id'))['id__count']
 
 
 def get_number_completed_modules_per_harambee(harambee):
     return HarambeeJourneyModuleRel.objects.filter(harambee=harambee, state=HarambeeJourneyModuleRel.MODULE_COMPLETE)\
-        .aggregate(Count('id'))['count__id']
+        .aggregate(Count('id'))['id__count']
 
 
 def get_level_achieved_in_module(harambee_journey_module_rel):
-    return HarambeeJourneyModuleLevelRel.objects.filter(harambee_journey_module_rel=harambee_journey_module_rel)\
-        .order_by('-level__order').first().order
+    rel = HarambeeJourneyModuleLevelRel.objects.filter(harambee_journey_module_rel=harambee_journey_module_rel)\
+        .order_by('-level__order').first()
+
+    if rel:
+        return rel.level.order
+    else:
+        return 0
 
 
 def get_level_time_by_harmabee(harambee, level):
     try:
         times = HarambeeeQuestionAnswerTime.objects.filter(harambee=harambee, harambee_level_rel__level=level)
-        total_time = 0;
+        if times.aggregate(Count('id'))['id__count'] == 0:
+            return 0
+        total_time = 0
         for time in times:
             total_time += time.answer_time_in_minutes()
         return total_time/times.aggregate(Count('id'))['id__count']
@@ -175,14 +192,18 @@ def get_average_level_time_per_module(harambee_jounrey_module_rel):
         if time > 0:
             total_time += time
             count += 1
+    if count == 0:
+        return 0
     return total_time/count
 
 
 def get_percentage_correct_in_level(harambee, level):
     num_answered = HarambeeQuestionAnswer.objects.filter(harambee=harambee, harambee_level_rel__level=level)\
-        .aggregate(Count('id'))['count_id']
+        .aggregate(Count('id'))['id__count']
+    if num_answered == 0:
+        return 0
     num_correct = HarambeeQuestionAnswer.objects.filter(harambee=harambee, harambee_level_rel__level=level,
-                                                        correct=True).aggregate(Count('id'))['count_id']
+                                                        option_selected=True).aggregate(Count('id'))['id__count']
     return num_correct/num_answered * 100
 
 
@@ -212,12 +233,12 @@ def get_platform_time(harambee):
     return total_time
 
 
-def get_number_logins(harambee):
-    return HarambeeLog.objects.filter(harambee=harambee, action=HarambeeLog.LOGIN).aggregate(Count('id'))['count_id']
-
-
-def get_number_logouts(harambee):
-    return HarambeeLog.objects.filter(harambee=harambee, action=HarambeeLog.LOGOUT).aggregate(Count('id'))['count_id']
+# def get_number_logins(harambee):
+#     return HarambeeLog.objects.filter(harambee=harambee, action=HarambeeLog.LOGIN).aggregate(Count('id'))['count_id']
+#
+#
+# def get_number_logouts(harambee):
+#     return HarambeeLog.objects.filter(harambee=harambee, action=HarambeeLog.LOGOUT).aggregate(Count('id'))['count_id']
 
 
 def create_json_stats():
@@ -256,8 +277,10 @@ def create_json_stats():
     all_harambees = Harambee.objects.all()
     for harambee in all_harambees:
         harambee_data = dict()
-        harambee_data['num_login'] = get_number_logins(harambee)
-        harambee_data['num_logout'] = get_number_logouts(harambee)
+        harambee_data['num_login'] = 0
+        harambee_data['num_logout'] = 0
+        # harambee_data['num_login'] = get_number_logins(harambee)
+        # harambee_data['num_logout'] = get_number_logouts(harambee)
         harambee_data['plat_time'] = get_platform_time(harambee)
         harambee_data['act_mod'] = get_number_active_modules_per_harambee(harambee)
         harambee_data['comp_mod'] = get_number_completed_modules_per_harambee(harambee)
@@ -271,10 +294,10 @@ def create_json_stats():
             module_data['lvl_perc_cor'] = get_percentage_correct_in_level_per_module(rel)
             module_data['mod_time'] = get_module_time(rel)
 
-            modules_list.append({'module_name': rel.module.name, 'module_data': module_data})
+            modules_list.append({'module_name': rel.journey_module_rel.module.name, 'module_data': module_data})
 
         harambees.append({'candidate_id': harambee.candidate_id, 'data': harambee_data, 'modules': modules_list})
 
     metrics['harambees'] = harambees
 
-    json.dumps([metrics])
+    return json.dumps([metrics])
