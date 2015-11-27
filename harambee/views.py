9fd9f11b0025538fba9b364f4418c0c0c6a73527
msinovcic <msinovcic@retrorabbit.co.za>
2015-11-27 09:53:32 +0200

    fixed test

--- harambee/views.py
+++ harambee/views.py
@@ -724,10 +724,6 @@ class WrongView(DetailView):
 
         return context
 
-    def get_context_data(self, **kwargs):
-        context = super(HelpView, self).get_context_data(**kwargs)
-        return context
-    
     def get_object(self, queryset=None):
         harambee = Harambee.objects.get(id=self.request.session["user"]["id"])
         return HarambeeState.objects.get(harambee=harambee).active_level_rel
@@ -746,6 +742,10 @@ class HelpView(ListView):
         pages = HelpPage.objects.filter(activate__lt=datetime.now()).filter(Q(deactivate__gt=datetime.now())
                                                                             | Q(deactivate=None))
         return pages
+
+    def get_context_data(self, **kwargs):
+        context = super(HelpView, self).get_context_data(**kwargs)
+        return context
 
 
 class HelpPageView(DetailView):
