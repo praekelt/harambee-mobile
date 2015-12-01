50258e900d8fcbf47b793ac8979514a81fc84270
msinovcic <msinovcic@retrorabbit.co.za>
2015-12-01 15:02:43 +0200

    Added Harambee filter

---
+++ my_auth/filters.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from my_auth.models import Harambee
+
+
+class HarambeeFilter(admin.SimpleListFilter):
+    title = _('User')
+    parameter_name = 'harambee'
+
+    def lookups(self, request, model_admin):
+        return [(x.id, x.first_name + ' ' + x.last_name) for x
+                in Harambee.objects.all().order_by('first_name', 'last_name')]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        else:
+            return queryset.filter(harambee__id=self.value)
