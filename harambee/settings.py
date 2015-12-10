4043bce1c203dcb5de802218d66846e06e359ab6
dbreedt <dbreedt@retrorabbit.co.za>
2015-12-10 08:10:58 +0200

    Changed the ES logic to 9200

--- harambee/settings.py
+++ harambee/settings.py
@@ -138,7 +138,7 @@ LOGIN_URL = '/login/'
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9201/',
+        'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': 'haystack',
     },
 }
