d02b8ca816b59f09bf1243f6d7a3dcb5a0e6509b
msinovcic <msinovcic@retrorabbit.co.za>
2016-01-07 09:37:04 +0200

    Updated help text for image field

---
+++ content/migrations/0009_auto_20160107_0936.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('content', '0008_auto_20160107_0836'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='levelquestion',
+            name='image',
+            field=models.ImageField(help_text=b"This is an image and the ideal size for this image should be between 150px and300px in width. If the image width is bigger or smaller the phone's browserwill scale it and the image will look very pixelated.", upload_to=b'questions/', null=True, verbose_name=b'Image', blank=True),
+        ),
+    ]
