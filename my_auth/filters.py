3d4d8eaa1d522054bc0d165d1db18cf4d44e89c1
msinovcic <msinovcic@retrorabbit.co.za>
2016-01-26 17:52:58 +0200

    Added filters

--- my_auth/filters.py
+++ my_auth/filters.py
@@ -18,4 +18,63 @@ class HarambeeFilter(admin.SimpleListFilter):
         if self.value() is None:
             return queryset
         else:
-            return queryset.filter(harambee__id=self.value)
+            return queryset.filter(harambee__id=self.value())
+
+
+class HarambeeActiveStatusFilter(admin.SimpleListFilter):
+    title = _('Status')
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [('active', 'Active'), ('inactive', 'Inactive')]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        else:
+            if self.value() == 'active':
+                now = timezone.now()
+                yesterday = timezone.now() - timedelta(hours=24)
+                harambee_list = Harambee.objects.filter(last_login__gt=yesterday, last_login__lt=now)\
+                    .values_list('id', flat=True)
+                return queryset.filter(id__in=harambee_list)
+            else:
+                two_weeks_ago = timezone.now() - timedelta(days=14)
+                harambee_list = Harambee.objects.filter(last_login__lt=two_weeks_ago)
+                return queryset.filter(id__in=harambee_list)
+
+
+class HaramabeeActiveInModule(admin.SimpleListFilter):
+    title = _('Active In Module')
+    parameter_name = 'act_mod'
+
+    def lookups(self, request, model_admin):
+        return [(x.id, x.module.name) for x in JourneyModuleRel.objects.all().order_by('module__name')]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        else:
+            harambee_list = HarambeeJourneyModuleRel.objects\
+                .filter(journey_module_rel__id=self.value(), state=HarambeeJourneyModuleRel.MODULE_ACTIVE)\
+                .distinct('harambee')\
+                .values_list('harambee__id', flat=True)
+            return queryset.filter(id__in=harambee_list)
+
+
+class HarambeeCompletedModule(admin.SimpleListFilter):
+    title = _('Completed Module')
+    parameter_name = 'comp_mod'
+
+    def lookups(self, request, model_admin):
+        return [(x.id, x.module.name) for x in JourneyModuleRel.objects.all().order_by('module__name')]
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        else:
+            harambee_list = HarambeeJourneyModuleRel.objects\
+                .filter(journey_module_rel__id=self.value(), state=HarambeeJourneyModuleRel.MODULE_COMPLETE)\
+                .distinct('harambee')\
+                .values_list('harambee__id', flat=True)
+            return queryset.filter(id__in=harambee_list)
