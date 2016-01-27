ffe24597c0ac3d9422df66226185976df0b2ef7d
msinovcic <msinovcic@retrorabbit.co.za>
2016-01-27 09:11:01 +0200

    Passing a string instead of model

--- communication/models.py
+++ communication/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Sms(models.Model):
     sent = models.BooleanField('Sent', default=False)
-    harambee = models.ForeignKey(Harambee, related_name='User', null=True, blank=False)
+    harambee = models.ForeignKey('my_auth.Harambee', related_name='User', null=True, blank=False)
     message = models.TextField('Message', blank=False)
     time_sent = models.DateTimeField('Time sent', null=True, blank=True)
     date_created = models.DateTimeField('Date created', auto_now_add=True, null=True, blank=True)
