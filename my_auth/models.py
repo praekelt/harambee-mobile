3720792e1b1644811a350b4f263daa0289815964
msinovcic <msinovcic@retrorabbit.co.za>
2016-01-27 09:12:01 +0200

    Importing SMS model

--- my_auth/models.py
+++ my_auth/models.py
@@ -7,6 +7,7 @@ from django.db.models import Count
 from django.contrib.auth.models import AbstractUser
 from content.models import Level, LevelQuestion, HarambeeQuestionAnswer, HarambeeJourneyModuleLevelRel, Module, \
     HarambeeeQuestionAnswerTime
+from communication.models import Sms
 
 
 class CustomUser(AbstractUser):
