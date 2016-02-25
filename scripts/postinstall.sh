57ad5a3a556adbd1e6035d5ebd32661ebca4cc13
Colin Alston <colin.alston@gmail.com>
2016-02-25 17:18:06 +0200

    Update index on post-install

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,7 @@
 manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
+pip install git+ssh://git@github.com/HarambeeYouth/HarambeeYouth.git
 
 $manage migrate --noinput
 cd ${INSTALLDIR}/${REPO}
 $manage collectstatic --noinput
-pip install git+ssh://git@github.com/HarambeeYouth/HarambeeYouth.git
+$manage update_index 
