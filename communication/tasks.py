b611f0519f67e4c5e42314ca138ab063afaf2f22
msinovcic <msinovcic@retrorabbit.co.za>
2016-03-02 18:07:00 +0200

    Added exception handling around sending and saving sms

--- communication/tasks.py
+++ communication/tasks.py
@@ -19,18 +19,19 @@ def send_smses():
     fail = 0
 
     for sms in smses:
-        sent = False
         if fail < 3:
             try:
                 send_sms(sms.harambee.candidate_id, sms.message)
-                sent = True
-            except (ValueError, httplib2.ServerNotFoundError):
+            except (Exception, ValueError, httplib2.ServerNotFoundError):
                 fail += 1
+                continue
 
-        if sent:
             sms.sent = True
             sms.time_sent = timezone.now()
-            sms.save()
+            try:
+                sms.save()
+            except:
+                fail += 1
 
 
 @task
